f14b6917120b3028954eec7f5be3e31819fc19bf
Orlando Cogo <orlando.cogo.o@gmail.com>
2026-03-05 15:19:36 +0100

    feat(server): add comment

--- server/server.js
+++ server/server.js
@@ -1,16 +1,13 @@
-//Variable + dependance
-
 //Express creer un serveur HTTP + gestion des routes
 const express = require('express');
 
 //app = serveur web --> represente le serveur Express
 const app = express();
 
-//app.get --> cree une route GET & / route racine
+//app.get --> cree une route GET and "/" route racine
 app.get('/', (req, res) => {
     res.json( 'Hello World!');
 })
-
 
 //Lancement du server sur le port 3000
 app.listen(3000, () => {
